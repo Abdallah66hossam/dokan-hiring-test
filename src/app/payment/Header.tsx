e1efd982aa4eeb1c52a54ceae556ec8052f533c2
Abdallah <abdallah@details.net.sa>
2025-08-12 11:13:52 +0300

    fixed responsive issue

--- src/app/payment/Header.tsx
+++ src/app/payment/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         <h1 className="leading-7 sm:leading-8 text-xl sm:text-2xl font-semibold text-header">
           Payment Details
         </h1>
-        <p className="text-[#EDEDED] leading-5 sm:leading-6 mt-2 text-sm sm:text-base sm:w-full w-[95%] animate-fade-in-delayed">
+        <p className="text-[#EDEDED] leading-5 sm:leading-6 mt-2 text-sm sm:text-base md:w-full sm:w-[95%] w-[90%] animate-fade-in-delayed">
           Complete your purchase by providing your payment details
         </p>
       </div>
@@ -17,7 +17,7 @@ const Header = () => {
         alt="Dokan logo"
         width={33}
         height={46}
-        className="w-[28px] sm:w-[33px] h-auto sm:relative absolute right-6 top-6 sm:top-auto sm:right-auto animate-logo-bounce"
+        className="w-[28px] sm:w-[33px] h-auto sm:relative absolute right-2 sm:right-6 top-0 sm:top-6 md:top-auto md:right-auto animate-logo-bounce"
       />
     </section>
   );
